ceeec0abe9ebee26a2cbf142b73a47daeffc782a
Serjo85 <bobrosebob07@gmail.com>
2021-04-05 11:20:18 +0300

    Add project files.

---
+++ VotesCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VotesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
