2a1a2babedae4b7f5fa130bc667f798ec3083a0c
Serjo85 <bobrosebob07@gmail.com>
2021-04-05 12:31:45 +0300

    FirstComment

---
+++ VotesCounter/Data/TestData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VotesCounter.Data
+{
+    public class TestData
+    {
+        private static Random r = new Random();
+        public static void GenerateTestData()
+        {
+            int blocksCount = 1;
+        }
+    }
+}
