7a481ebfa4e7e658e17621c83e8a882f3b3fe924
Serjo85 <bobrosebob07@gmail.com>
2021-04-14 15:52:34 +0300

    Добавлен тест сервис

---
+++ VotesCounter/Services/TestDataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VotesCounter.Model;
+
+namespace VotesCounter.Services
+{
+    public class TestDataService
+    {
+        public static void CreateFile(List<VoteData> items)
+        {
+            var _items = items;
+            List<string> s = new();
+            s.Add(_items.Count.ToString());
+            s.Add("");
+            foreach (var item in _items)
+            {
+                s.Add(item.canCount.ToString());
+                foreach (var name in item.Names)
+                {
+                    s.Add(name);
+                }
+
+                string bull = "";
+                for (int i = 0; i < item.Bulletins.GetLength(0); i++)
+                {
+                    for (int j = 0; j < item.Bulletins.GetLength(1); j++)
+                    {
+                        bull += item.Bulletins[i, j].ToString();
+                        if (j != item.Bulletins.GetLength(1)) bull += " ";
+                    }
+                    s.Add(bull);
+                    bull = "";
+                }
+                s.Add("");
+            }
+
+            try
+            {
+                System.IO.File.WriteAllLines("randomfile.txt", s);
+                Console.WriteLine("Файл успешно создан!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Файл не создан, ошибка:");
+                Console.WriteLine(e);
+            }
+    }
+}
